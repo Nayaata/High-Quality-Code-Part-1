7370a6e0dd9928e7654422cc0cf4ecf07e31dcb2
NayaIt <n.m.keranova@gmail.com>
2016-08-25 02:54:01 +0300

    Upload demo version of CodeFormatting homework

---
+++ CodeFormatting/CodeFormatting/Bunny.cs
@@ -0,0 +1,49 @@
+﻿namespace CodeFormatting
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using CodeFormatting.Contracts;
+
+    /// <summary>
+    /// Bunny class 
+    /// </summary>
+    [Serializable]
+    public class Bunny
+    {
+        public int Age { get; set; }
+
+        public string Name { get; set; }
+
+        public FurType FurType { get; set; }
+
+        /// <summary>
+        /// Introduce method show the main information for each Bunny
+        /// </summary>
+        /// <param name="writer">IWriter writer</param>
+        public void Introduce(IWriter writer)
+        {
+            writer.WriteLine(this.Name + " - \"I am " + this.Age + " years old!");
+            writer.WriteLine(this.Name + " - \"And I am " + this.FurType.ToString().SplitToSeparateWordsByUppercaseLetter());
+        }
+
+        /// <summary>
+        /// ToString method print the information on the Console
+        /// </summary>
+        /// <returns>builder.ToString()</returns>
+        public override string ToString()
+        {
+            var builderSize = 200;
+
+            var builder = new StringBuilder(builderSize);
+
+            builder.AppendLine("Bunny name: " + this.Name);
+            builder.AppendLine("Bunny age: " + this.Age);
+            builder.AppendLine("Bunny fur: " + this.FurType.ToString().SplitToSeparateWordsByUppercaseLetter());
+
+            return builder.ToString();
+        }
+    }
+}
---
+++ CodeFormatting/CodeFormatting/Contracts/IWriter.cs
@@ -0,0 +1,26 @@
+﻿namespace CodeFormatting.Contracts
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// Interface IWriter
+    /// </summary>
+    public interface IWriter
+    {
+        /// <summary>
+        /// Write method in the interface wich must be inplement
+        /// </summary>
+        /// <param name="message">string message</param>
+       public void Write(string message);
+
+        /// <summary>
+       /// WriteLine method in the interface wich must be inplement
+        /// </summary>
+       /// <param name="message">string message</param>
+       public void WriteLine(string message);
+    }
+}
